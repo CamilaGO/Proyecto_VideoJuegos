68f28bda3390f979a9cf5653ca82ecf69b23ded5
Camila <gon18398@uvg.edu.gt>
2019-05-22 23:35:38 -0600

    avances en pong

---
+++ Assets/Scripts/ScriptsPong/Barra.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barra : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Init(bool isRight)
+    {
+        Vector2 posicion = Vector2.zero;
+        if (isRight == true)
+        {
+            //se coloca del lado derechoa
+            posicion = new Vector2(PongManager.topDer.x, 0);
+            posicion += Vector2.right * transform.localScale.x;
+        } else
+        {
+            //se coloca del lado izquierdo
+            posicion = new Vector2(PongManager.bottomIzq.x, 0);
+            posicion += Vector2.right * transform.localScale.x;
+        }
+
+        transform.position = posicion;
+    }
+}
---
+++ Assets/Scripts/ScriptsPong/PongManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PongManager : MonoBehaviour
+{
+    public Pelota pelota;
+    public Barra barra;
+
+    public static Vector2 bottomIzq;
+    public static Vector2 topDer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bottomIzq = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+        topDer = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        
+        //Se crean 2 barras y 1 pelota al iniciar el juego
+        Instantiate(pelota);
+        Barra barra1 = Instantiate(barra) as Barra;
+        Barra barra2 = Instantiate(barra) as Barra;
+        barra1.Init(true);  //barra derecha
+        barra1.Init(false);   //barra izquierda
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
